e1228affeb68e45bce71a6fbbcdb8abf3eda5302
Jesse Maretz <jesse.maretz@gmail.com>
2020-11-09 19:58:58 -0500

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const Player = require("./models/Player")
 const parser = require("body-parser")
+const cors = require("cors")
+
+app.use(function (req, res, next) {  res.header("Access-Control-Allow-Methods", "POST,GET,PATCH,PUT,DELETE");  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  res.header("Access-Control-Allow-Credentials", "true");  res.header("Access-Control-Allow-Origin", "*");  next();});
 
 app.use(parser.json())
 
